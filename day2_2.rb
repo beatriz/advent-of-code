3c242032b559aea4062280b065830bfb9d2e6474
Beatriz Magalhaes <beatriz.magalhaes@farfetch.com>
2017-12-07 09:40:35 +0000

    Completed second part of day 2

--- day2_2.rb
+++ day2_2.rb
@@ -4,9 +4,15 @@ checksum = 0
 line = ""
 while line != 'exit'
     line = gets.chomp
-    numbers = line.split.map(&:to_i).sort
-    
-    checksum += numbers.max - numbers.min
+    numbers = line.split.map(&:to_i).sort{ |x,y| y <=> x }
+    for i in 0...numbers.length
+        for j in (i+1)...numbers.length
+            if numbers[i] % numbers[j] == 0
+                checksum += (numbers[i] / numbers[j])
+                break
+            end
+        end
+    end
 end
 
 puts checksum
