7809d4c6ceadd6ec4a0ee30c5dca660763072276
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2018-12-03 10:39:11 +0000

    Fix day 2 problem 2 solution

--- 2018/src/main/scala/com/mbeatrizmagalhaes/adventofcode/Problem_22.scala
+++ 2018/src/main/scala/com/mbeatrizmagalhaes/adventofcode/Problem_22.scala
@@ -4,15 +4,16 @@ class Problem_22 extends Problem {
   implicit def bool2int(b:Boolean): Int = if (b) 1 else 0
 
   override def solve(lines: List[String]) = {
-    val (word, char) = find(lines.head, lines.tail)
-    word.replace(char, "")
+    val (word1, word2) = find(lines.head, lines.tail)
+    word1 + " " + word2
   }
 
   def find(current: String, remaining: List[String]): (String, String) = {
-    val diffList = remaining.map(x => (x, x.diff(current)))
-    diffList.find(_._2.length == 1) match {
-      case Some(str) => str
-      case None => find(remaining.head, remaining.tail)
-    }
+    remaining
+      .map(x => (x, (x zip current).filter(t => t._1 != t._2)))
+      .find(_._2.length == 1) match {
+        case Some(str) => (current, str._1)
+        case None => find(remaining.head, remaining.tail)
+      }
   }
 }
