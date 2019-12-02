2e947fc3369f4f05a555b8fedb96fd6b87f76c63
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2019-12-02 13:09:16 +0000

    Improve day1 solution

--- 2019/da1.hs
+++ 2019/da1.hs
@@ -1,9 +1,10 @@
-requiredFuel1 :: Int -> Int
-requiredFuel1 mass = mass `div` 3 - 2
+calculateBasicRequiredFuel :: Int -> Int
+calculateBasicRequiredFuel mass = mass `div` 3 - 2
 
-requiredFuel mass = 
-  let nextMass = requiredFuel1 mass
-  in if requiredFuel1 nextMass < 0 then nextMass else nextMass + requiredFuel nextMass
+calculateRequiredFuel mass = 
+  let requiredF = calculateBasicRequiredFuel mass
+      nextRequiredF = if calculateBasicRequiredFuel requiredF < 0 then 0 else calculateRequiredFuel requiredF
+  in requiredF + nextRequiredF
 
 toInt :: [String] -> [Int]
 toInt values = map read values
@@ -11,4 +12,4 @@ toInt values = map read values
 main = do
   file <- readFile "./input/day1.txt"
   let values = toInt (lines file)
-  print $ sum (map requiredFuel values)
+  print $ sum (map calculateRequiredFuel values)
