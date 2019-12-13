a57605a740a9722c03699d5069b0e460c3804f81
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2019-12-13 12:16:35 +0000

    Move intcode to separate file and reuse it

--- 2019/day5.js
+++ 2019/day5.js
@@ -1,77 +1,9 @@
-let _ = require('lodash');
 let fs = require('fs')
-
-const shouldEnd = (arr, to) => to > arr.length - 1
-
-const getInstructions = (str) => ({
-  opcode: str % 100,
-  param1: Math.floor(str / 100) % 10 || 0,
-  param2: Math.floor(str / 1000) % 10 || 0,
-  param3: Math.floor(str / 10000) % 10 || 0,
-  size: ('' + str).length
-})
-
-const work = (nums, input, offset = 0) => {
-  const inst = getInstructions(nums[offset]);
-
-  const getVal = (n) => {
-    const pos = nums[offset + n];
-    switch (inst[`param${n}`]){
-      case 0:
-        return nums[pos];
-      case 1:
-        return pos;
-      default:
-        console.log('invalid parameter')
-        return 'xxx';
-    }
-  }
-
-  if (inst.opcode === 99){
-    return nums;
-  }
-  
-  let advance = 0;
-  
-  if (inst.opcode === 1 || inst.opcode === 2){
-    const val1 = getVal(1)
-    const val2 = getVal(2)
-    
-    const pos3 = nums[offset + 3];
-    nums[pos3] = inst.opcode === 1 ? val1 + val2 : val1 * val2;
-    advance = 4;
-  }
-  else if (inst.opcode === 3){
-    const pos = nums[offset + 1];
-    nums[pos] = input;
-    advance = 2;
-  }
-  else if (inst.opcode === 4) {
-    console.log(getVal(1));
-    advance = 2;
-  }
-  else if (inst.opcode <= 6) {
-    const val = getVal(1)
-    
-    if ((inst.opcode == 5 && val != 0) || (inst.opcode == 6 && val == 0)){
-      offset = getVal(2)
-    } else {
-      advance = 3
-    }
-  } else if (inst.opcode <= 8) {
-    const val1 = getVal(1)
-    const val2 = getVal(2)
-    const pos3 = nums[offset + 3];
-    nums[pos3] = + (inst.opcode == 7 && val1 < val2) || (inst.opcode == 8 && val1 == val2)
-    advance = 4
-  }
-  
-  return shouldEnd(nums, offset + advance) ? nums : work(nums, input, offset + advance);
-}
+let Intcode = require('./intcode')
 
 const x = fs.readFileSync('./input/day5.txt', "utf8");
 const nums = x.split(',').map(v => Number(v))
 console.log('Part 1')
-work(nums.slice(), 1)
+Intcode.run(nums.slice(), 1)
 console.log('Part2')
-work(nums, 5)
+Intcode.run(nums.slice(), 5)
---
+++ 2019/intcode.js
@@ -0,0 +1,97 @@
+let _ = require('lodash');
+let fs = require('fs')
+
+const shouldEnd = (arr, to) => to > arr.length - 1
+
+const getInstructions = (str) => ({
+  opcode: str % 100,
+  param1: Math.floor(str / 100) % 10 || 0,
+  param2: Math.floor(str / 1000) % 10 || 0,
+  param3: Math.floor(str / 10000) % 10 || 0,
+  size: ('' + str).length
+})
+
+const work = (nums, input, relativeBase = 0, offset = 0) => {
+  const inst = getInstructions(nums[offset]);
+  const getVal = (n) => {
+    const pos = nums[offset + n] || 0;
+    const notFromPos = n === 3 || inst.opcode === 3;
+    switch (inst[`param${n}`]){
+      case 0:
+        return notFromPos ? pos : nums[pos] || 0;
+      case 1:
+        return pos || 0;
+      case 2:
+        return notFromPos ? pos + relativeBase : nums[pos + relativeBase] || 0;
+      default:
+        console.log('invalid parameter')
+        return {end: true};
+    }
+  }
+
+  if (inst.opcode === 99){
+    return {end: true};
+  }
+  
+  let advance = 0;
+  let newRelativeBase = relativeBase;
+  
+  if (inst.opcode === 1 || inst.opcode === 2){
+    const val1 = getVal(1)
+    const val2 = getVal(2)
+    
+    const pos3 = getVal(3)
+    nums[pos3] = inst.opcode === 1 ? val1 + val2 : val1 * val2;
+    advance = 4;
+  }
+  else if (inst.opcode === 3){
+    const pos = getVal(1);
+    nums[pos] = input;
+    advance = 2;
+  }
+  else if (inst.opcode === 4) {
+    console.log(getVal(1));
+    advance = 2;
+  }
+  else if (inst.opcode <= 6) {
+    const val = getVal(1)
+    
+    if ((inst.opcode == 5 && val != 0) || (inst.opcode == 6 && val == 0)){
+      offset = getVal(2)
+    } else {
+      advance = 3
+    }
+  } else if (inst.opcode <= 8) {
+    const val1 = getVal(1)
+    const val2 = getVal(2)
+    const pos3 = getVal(3);
+    const valBool = (inst.opcode == 7 && val1 < val2) || (inst.opcode == 8 && val1 == val2)
+    nums[pos3] = valBool ? 1 : 0  
+    advance = 4
+  }
+  else if (inst.opcode === 9) {
+    const val = getVal(1);
+    newRelativeBase += val;
+    advance = 2;
+  }
+  
+  return { nums, newRelativeBase, newOffset: offset + advance };
+}
+
+const run = (n, input) => {
+  let relativeBase, offset = 0;
+  let output = [];
+  let nums = n;
+  while (true) {
+    const res = work(nums, input, relativeBase, offset);
+    if (res.output !== undefined) { output.push(res.output); }
+    if (res.end === true) {
+      break;
+    }
+    nums = res.nums;
+    relativeBase = res.newRelativeBase;
+    offset = res.newOffset;
+  }
+}
+
+module.exports = {run, work}
