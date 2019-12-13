251eb73b23adcfb920c3a539c8913055e1320581
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2019-12-13 12:03:28 +0000

    Add solutions to day 9

--- 2019/day9.js
+++ 2019/day9.js
@@ -11,38 +11,41 @@ const getInstructions = (str) => ({
   size: ('' + str).length
 })
 
-const work = (nums, input, offset = 0) => {
+const work = (nums, input, relativeBase = 0, offset = 0) => {
   const inst = getInstructions(nums[offset]);
-
   const getVal = (n) => {
-    const pos = nums[offset + n];
+    const pos = nums[offset + n] || 0;
+    const notFromPos = n === 3 || inst.opcode === 3;
     switch (inst[`param${n}`]){
       case 0:
-        return nums[pos];
+        return notFromPos ? pos : nums[pos] || 0;
       case 1:
-        return pos;
+        return pos || 0;
+      case 2:
+        return notFromPos ? pos + relativeBase : nums[pos + relativeBase] || 0;
       default:
         console.log('invalid parameter')
-        return 'xxx';
+        return {end: true};
     }
   }
 
   if (inst.opcode === 99){
-    return nums;
+    return {end: true};
   }
   
   let advance = 0;
+  let newRelativeBase = relativeBase;
   
   if (inst.opcode === 1 || inst.opcode === 2){
     const val1 = getVal(1)
     const val2 = getVal(2)
     
-    const pos3 = nums[offset + 3];
+    const pos3 = getVal(3)
     nums[pos3] = inst.opcode === 1 ? val1 + val2 : val1 * val2;
     advance = 4;
   }
   else if (inst.opcode === 3){
-    const pos = nums[offset + 1];
+    const pos = getVal(1);
     nums[pos] = input;
     advance = 2;
   }
@@ -61,17 +64,39 @@ const work = (nums, input, offset = 0) => {
   } else if (inst.opcode <= 8) {
     const val1 = getVal(1)
     const val2 = getVal(2)
-    const pos3 = nums[offset + 3];
-    nums[pos3] = + (inst.opcode == 7 && val1 < val2) || (inst.opcode == 8 && val1 == val2)
+    const pos3 = getVal(3);
+    const valBool = (inst.opcode == 7 && val1 < val2) || (inst.opcode == 8 && val1 == val2)
+    nums[pos3] = valBool ? 1 : 0  
     advance = 4
   }
+  else if (inst.opcode === 9) {
+    const val = getVal(1);
+    newRelativeBase += val;
+    advance = 2;
+  }
   
-  return shouldEnd(nums, offset + advance) ? nums : work(nums, input, offset + advance);
+  return { nums, newRelativeBase, newOffset: offset + advance };
 }
 
-const x = fs.readFileSync('./input/day5.txt', "utf8");
+const run = (n, input) => {
+  let relativeBase, offset = 0;
+  let output = [];
+  let nums = n;
+  while (true) {
+    const res = work(nums, input, relativeBase, offset);
+    if (res.output !== undefined) { output.push(res.output); }
+    if (res.end === true) {
+      break;
+    }
+    nums = res.nums;
+    relativeBase = res.newRelativeBase;
+    offset = res.newOffset;
+  }
+}
+
+const x = fs.readFileSync('./input/day9.txt', "utf8");
 const nums = x.split(',').map(v => Number(v))
 console.log('Part 1')
-work(nums.slice(), 1)
+run(nums.slice(), 1)
 console.log('Part2')
-work(nums, 5)
+run(nums.slice(), 2)
