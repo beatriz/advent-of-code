fcd6ae0d73f2d763f5dde7935e918f29e454ca71
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2020-12-01 15:21:04 +0000

    Add solutions for day 1 (2020)

---
+++ 2020/src/main/scala/com/beatriz/adventofcode/Day1.scala
@@ -0,0 +1,21 @@
+package adventofcode
+
+class Day1 extends Problem[Int, Int] {
+
+  def solve1(values: Seq[Int]): (Int, Int) = values match {
+    case Nil => (0,0)
+    case h :: t =>
+      t.map(x => (x, x + h)).find(_._2 == 2020) match {
+        case Some((x, s)) => (h, x)
+        case None => solve1(t)
+      }
+  }
+  
+  def solve(input: Seq[String]) = {
+    val lines = input.map(_.toInt)
+
+    val res1 = solve1(lines)
+    (res1._1 * res1._2, 
+      lines.combinations(3).toList.map(x => (x, x.sum)).find(_._2 == 2020).map(_._1.product).getOrElse(0))
+  }
+} 
