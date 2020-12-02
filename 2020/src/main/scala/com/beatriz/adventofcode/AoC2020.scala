d22006b92e28f400bd0d90d936a635fda4c4df16
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2020-12-02 10:01:54 +0000

    Add ability to run with test file

--- 2020/src/main/scala/com/beatriz/adventofcode/AoC2020.scala
+++ 2020/src/main/scala/com/beatriz/adventofcode/AoC2020.scala
@@ -1,13 +1,18 @@
 package adventofcode
 
 import scala.io.Source
+import scala.util.Try
 
 object AoC2020 extends App {
   val problemN: String = args(0)
   val problem = Class.forName(s"adventofcode.Day$problemN")
-  .newInstance().asInstanceOf[Problem[_, _]]
+    .newInstance().asInstanceOf[Problem[_, _]]
+  val isTest = Try(args(1)).toOption.isDefined
 
-  val input: Seq[String] = Source.fromFile(s"input/day$problemN.txt").getLines.toSeq
+  val input: Seq[String] = {
+    val fileName = if(isTest) "test" else "day" + problemN
+    Source.fromFile(s"input/$fileName.txt").getLines.toSeq
+  }
 
   val (res1, res2) = problem.solve(input)
 
