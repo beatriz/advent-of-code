56727dc78574ad91d2e7f1353346999b854f2023
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2020-12-08 11:03:29 +0000

    Add solution for day 8/2020 part 2

--- 2020/src/main/scala/adventofcode/Day8.scala
+++ 2020/src/main/scala/adventofcode/Day8.scala
@@ -1,24 +1,62 @@
 package adventofcode
 
 class Day8 extends Problem[Int, Int] {
-  def findLoop(lines: Array[String], acc: Int = 0, currIndex: Int = 0, visited: Set[Int] = Set.empty): Int = {
-    if (visited.contains(currIndex)) acc
+  case class Command(line: String) {
+    val (operation, argument) = {
+      val Array(o, a) = line.split(" ")
+      (o, a.toInt)
+    }
+
+    def changeLine(newOp: String) =
+      if (newOp == "nop") line.replace("nop", "jmp")
+      else line.replace("jmp", "nop")
+  }
+
+  def findLoop(
+      lines: Array[String],
+      acc: Int = 0,
+      currIndex: Int = 0,
+      visited: Map[Int, Command] = Map.empty
+  ): (Int, Map[Int, Command], Boolean) = {
+
+    if (currIndex == lines.size - 1) (acc, visited, true)
+    else if (visited.contains(currIndex)) (acc, visited, false)
     else {
-      val line = lines(currIndex)
-      val Array(operation, argument) = line.split(" ")
-      operation match {
-        case "nop" => findLoop(lines, acc, currIndex + 1, visited + currIndex)
-        case "acc" => findLoop(lines, acc + argument.toInt, currIndex + 1, visited + currIndex)
-        case "jmp" => findLoop(lines, acc, currIndex + argument.toInt, visited + currIndex)
+      val command = Command(lines(currIndex))
+      command.operation match {
+        case "nop" => findLoop(lines, acc, currIndex + 1, visited + (currIndex -> command))
+        case "acc" => findLoop(lines, acc + command.argument, currIndex + 1, visited + (currIndex -> command))
+        case "jmp" => findLoop(lines, acc, currIndex + command.argument, visited + (currIndex -> command))
         case other =>
           println("invalid instruction: " + other)
-          -1
+          (-1, visited, false)
       }
     }
   }
 
-  def solve(input: String) = {
+  def solve2(lines: Array[String]): Int = {
+    val (_, initialVisited, _) = findLoop(lines)
 
-    (findLoop(input.split("\n")), 0)
+    def changeInstructions(notChanged: Set[Int]): Int = {
+      val index = notChanged.last
+      val commandToChange = initialVisited.get(index).get
+      commandToChange.operation match {
+        case op @ ("nop" | "jmp") =>
+          val newLine = commandToChange.changeLine(op)
+          val (acc, _, reachedLastOp) = findLoop(lines.updated(index, newLine))
+          if (reachedLastOp) acc
+          else changeInstructions(notChanged.init)
+        case "acc" =>
+          changeInstructions(notChanged.init)
+      }
+    }
+
+    changeInstructions(initialVisited.keySet)
+  }
+
+  def solve(input: String) = {
+    val lines = input.split("\n")
+    val (res1, _, _) = findLoop(lines)
+    (res1, solve2(lines))
   }
 }
