e4da4983f6d6e272e31383dedfb8ca8f9d591ab3
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2020-12-09 08:45:11 +0000

    Small improvements on day8/2020

--- 2020/src/main/scala/adventofcode/Day8.scala
+++ 2020/src/main/scala/adventofcode/Day8.scala
@@ -12,21 +12,23 @@ class Day8 extends Problem[Int, Int] {
       else line.replace("jmp", "nop")
   }
 
-  def findLoop(
+  def runUntilLastOperationOrLoop(
       lines: Array[String],
       acc: Int = 0,
       currIndex: Int = 0,
       visited: Map[Int, Command] = Map.empty
   ): (Int, Map[Int, Command], Boolean) = {
-
     if (currIndex == lines.size - 1) (acc, visited, true)
     else if (visited.contains(currIndex)) (acc, visited, false)
     else {
       val command = Command(lines(currIndex))
       command.operation match {
-        case "nop" => findLoop(lines, acc, currIndex + 1, visited + (currIndex -> command))
-        case "acc" => findLoop(lines, acc + command.argument, currIndex + 1, visited + (currIndex -> command))
-        case "jmp" => findLoop(lines, acc, currIndex + command.argument, visited + (currIndex -> command))
+        case "nop" =>
+          runUntilLastOperationOrLoop(lines, acc, currIndex + 1, visited + (currIndex -> command))
+        case "acc" =>
+          runUntilLastOperationOrLoop(lines, acc + command.argument, currIndex + 1, visited + (currIndex -> command))
+        case "jmp" =>
+          runUntilLastOperationOrLoop(lines, acc, currIndex + command.argument, visited + (currIndex -> command))
         case other =>
           println("invalid instruction: " + other)
           (-1, visited, false)
@@ -35,7 +37,7 @@ class Day8 extends Problem[Int, Int] {
   }
 
   def solve2(lines: Array[String]): Int = {
-    val (_, initialVisited, _) = findLoop(lines)
+    val (_, initialVisited, _) = runUntilLastOperationOrLoop(lines)
 
     def changeInstructions(notChanged: Set[Int]): Int = {
       val index = notChanged.last
@@ -43,7 +45,7 @@ class Day8 extends Problem[Int, Int] {
       commandToChange.operation match {
         case op @ ("nop" | "jmp") =>
           val newLine = commandToChange.changeLine(op)
-          val (acc, _, reachedLastOp) = findLoop(lines.updated(index, newLine))
+          val (acc, _, reachedLastOp) = runUntilLastOperationOrLoop(lines.updated(index, newLine))
           if (reachedLastOp) acc
           else changeInstructions(notChanged.init)
         case "acc" =>
@@ -56,7 +58,7 @@ class Day8 extends Problem[Int, Int] {
 
   def solve(input: String) = {
     val lines = input.split("\n")
-    val (res1, _, _) = findLoop(lines)
+    val (res1, _, _) = runUntilLastOperationOrLoop(lines)
     (res1, solve2(lines))
   }
 }
