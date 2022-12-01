ab3f7f2147d167a844c56ff7040adb039ac6b65a
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2022-12-01 17:06:42 +0000

    Setup 2022

---
+++ 2022/src/main/scala/adventofcode/2022/Main.scala
@@ -0,0 +1,24 @@
+package adventofcode.`2022`
+
+import scala.io.Source
+
+@main def hello(problemN: String, remainingArgs: String*): Unit =
+  // Scala3 still doesn't have support for optional arguments
+  val isTest = remainingArgs.headOption.map(_.toBoolean).getOrElse(false)
+  
+  val problem = Class.forName(s"adventofcode.2022.Day$problemN").newInstance().asInstanceOf[Problem[_, _]]
+
+  val file = 
+    val fileName = if(isTest) "test" else "day" + problemN
+    Source.fromFile(s"input/$fileName.txt")
+  
+  val input = file.mkString
+  file.close()
+  val (res1, res2) = problem.solve(input)
+
+
+  println(s"Day $problemN Part 1: $res1")
+  println(s"Day $problemN Part 2: $res2")
+
+trait Problem[A, B]:
+  def solve(input: String): (A, B)
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ These are my solutions to the [Advent of Code](https://adventofcode.com/) proble
 I've been solving as many problems as I can during the duration of the month of December and, sometimes, come back later to solve some more.
 
 I don't use the same programming language every year, these are the used ones per year so far:
+- [2022](/2022): Scala
 - [2021](/2021): TypeScript
 - [2020](/2020): Scala
 - [2019](/2019): Javascript
