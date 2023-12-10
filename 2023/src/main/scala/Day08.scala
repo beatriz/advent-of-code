a52310d0c462c31409dab628fb2fb5e72ce248b8
Beatriz Magalhães <bm@famly.co>
2023-12-10 09:39:52 +0000

    Add solution to part 1 of day 8/2023

---
+++ 2023/src/main/scala/Day08.scala
@@ -0,0 +1,26 @@
+object Day08 extends Problem[Int, Int]:
+  def traverseNodes(nodes: Map[String, (String, String)], directions: String, isStartingNode: String => Boolean, isEndNode: String => Boolean) =
+    def traverseAux(currNodes: List[String], dir: List[Char], count: Int): Int =
+      if (currNodes.forall(isEndNode)) count
+      else {
+        val nextNodes = currNodes.map{ currNode =>
+          val nextPair = nodes.get(currNode).get
+          if (dir.head == 'L') nextPair._1 else nextPair._2
+        }
+        traverseAux(nextNodes, if (dir.tail.isEmpty) directions.toList else dir.tail, count + 1)
+      }
+
+    traverseAux(nodes.keySet.filter(isStartingNode).toList, directions.toList, 0)
+
+  def solve(input: String): (Int, Int) =
+    val lines = getLines(input)
+    val instructions = lines.head
+
+    val nodes = lines.drop(2).map { line =>
+      val Array(key, pair) = line.split(" = ")
+      val Array(l, r) = pair.split(", ")
+      key -> (l.drop(1), r.dropRight(1))
+    }.toMap
+
+
+    (traverseNodes(nodes, instructions, _ == "AAA", _ == "ZZZ"), traverseNodes(nodes, instructions, _.endsWith("A"), _.endsWith("Z")))
